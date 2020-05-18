98f557d8172f65561ff45d599cf598450a7e52e1
Daddy <theygg@gmail.com>
2020-05-17 17:32:31 -0700

    Чистка кода 1

--- CubSpline/MainWindow.cs
+++ CubSpline/MainWindow.cs
@@ -9,8 +9,6 @@ namespace WindowsFormsApp1
     public partial class MainWindow : Form
     {
         private PointsInputForm inputDataForm = new PointsInputForm();
-        private EnterPolynomialForm polynomialInput = new EnterPolynomialForm();
-
         OpenFileDialog openFileDialogue = new OpenFileDialog();
 
         public MainWindow()
@@ -24,7 +22,7 @@ namespace WindowsFormsApp1
 
         private Series updateChartSeries(string name, Data.Point[] points)
         {
-            Series series = chart1.Series.FindByName(name);
+            Series series = chart.Series.FindByName(name);
             series.Points.Clear();
             foreach (var p in points)
             {
@@ -32,7 +30,7 @@ namespace WindowsFormsApp1
             }
             return series;
         }
-        private void Manual_menu_item_Click(object sender, EventArgs e)
+        private void EnterEditMenuItem_Click(object sender, EventArgs e)
         {
 
             if (inputDataForm.ShowDialog(this) == DialogResult.OK)
@@ -45,67 +43,7 @@ namespace WindowsFormsApp1
                 Data.Point[] splinePoints = spline.getPoints();
                 updateChartSeries("spline", splinePoints);
 
-                Data.FirstDerivative firstDerivative = new Data.FirstDerivative(spline.getPoints());
-                updateChartSeries("firstDerivative", firstDerivative.GetPoints()).Enabled = false;
-                showFirstDerivative.Checked = false;
-
-                Data.SecondDerivative secondDerivative = new Data.SecondDerivative(spline.getPoints());
-                updateChartSeries("secondDerivative", secondDerivative.GetPoints()).Enabled = false;
-                showSecondDerivative.Checked = false;
-                chart1.ChartAreas[0].AxisX.RoundAxisValues();
             }
-
-
-        }
-
-        private void showPolynomial_CheckedChanged(object sender, EventArgs e)
-        {
-            if (!polynomialInput.hasData)
-            {
-                chart1.Series["polynomial"].Enabled = false;
-                showPolynomial.Checked = false;
-            }
-            chart1.Series["polynomial"].Enabled = showPolynomial.Checked;
-            chart1.ResetAutoValues();
-        }
-
-        private void enterPolynomialToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (polynomialInput.ShowDialog(this) == DialogResult.OK)
-            {
-
-                Data.Polynomial p = polynomialInput.getPolynomial();
-                Series series = updateChartSeries("polynomial", p.getPoints(polynomialInput.rangeStart, polynomialInput.rangeEnd, polynomialInput.xOffset));
-                series.LegendText = p.ToString();
-                series.Enabled = true;
-                showPolynomial.Checked = true;
-                chart1.ChartAreas[0].AxisX.RoundAxisValues();
-                chart1.ResetAutoValues();
-            }
-
-        }
-
-        private void showSpline_CheckedChanged(object sender, EventArgs e)
-        {
-            Series series = chart1.Series.FindByName("spline");
-            series.Enabled = showSpline.Checked;
-            chart1.ChartAreas[0].AxisX.RoundAxisValues();
-            chart1.ResetAutoValues();
-        }
-
-        private void showFirstDerivative_CheckedChanged(object sender, EventArgs e)
-        {
-            Series series = chart1.Series.FindByName("firstDerivative");
-            series.Enabled = showFirstDerivative.Checked;
-            chart1.ChartAreas[0].AxisX.RoundAxisValues();
-            chart1.ResetAutoValues();
-        }
-
-        private void showSecondDerivative_CheckedChanged(object sender, EventArgs e)
-        {
-            Series series = chart1.Series.FindByName("secondDerivative");
-            series.Enabled = showSecondDerivative.Checked;
-            chart1.ResetAutoValues();
         }
     }
 }
